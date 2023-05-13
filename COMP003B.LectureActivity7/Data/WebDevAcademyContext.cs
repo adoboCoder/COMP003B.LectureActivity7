2b7edd35c70838ce472680c4f264f82609ba07f7
Jonathan Cruz <jcruz@adobo.dev>
2023-05-13 16:30:10 -0700

    Add project files.

---
+++ COMP003B.LectureActivity7/Data/WebDevAcademyContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.LectureActivity7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.LectureActivity7.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ COMP003B.LectureActivity7/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity7.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        // Collection navigation property
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+
+        public int Age { get; set; } // New property
+
+    }
+}
